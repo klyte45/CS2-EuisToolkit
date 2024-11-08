39ef28bb9f2225ed759d553e452da3c6f1174cbf
Leandro Klyte <klyte45@hotmail.com>
2024-11-07 22:58:23 -0300

    Converting to functional style react

--- euis-components/src/components/MainSideTabMenuComponent.tsx
+++ euis-components/src/components/MainSideTabMenuComponent.tsx
@@ -1,4 +1,4 @@
-import { Component, ReactNode } from "react";
+import { Component, ReactNode, useState } from "react";
 import { Tabs, TabList, Tab, TabPanel } from "react-tabs";
 import "../styles/mainSideTabs.scss";
 import { GameScrollComponent } from "./GameScrollComponent";
@@ -21,29 +21,27 @@ type State = {
     expanded?: boolean
 }
 
-export class MainSideTabMenuComponent extends Component<Props, State> {
-
-    render(): ReactNode {
-        return <>
-            <Tabs className={`mainTabsContainer ${this.state?.expanded ? "expanded" : ""}`} onSelect={() => this.setState({ expanded: false })}>
-                <div onClick={x => this.setState({ expanded: !this.state?.expanded })} className="topMainMenuButton" data-tooltip={this.props.tooltip ?? this.props.modTitle} data-tootip-position="bottom left">
-                    <img src={this.props.mainIconUrl} className="tabItemIcon" />
-                    <div className="titleContainer">
-                        <div className="tabName">{this.props.modTitle}</div>
-                        <div className="tabSubName">{this.props.subtitle}</div>
-                    </div>
+export const MainSideTabMenuComponent = (props: Props) => {
+    const [expanded, setExpanded] = useState(false);
+    return <>
+        <Tabs className={`mainTabsContainer ${expanded ? "expanded" : ""}`} onSelect={() => setExpanded(false)}>
+            <div onClick={x => setExpanded(!expanded)} className="topMainMenuButton" data-tooltip={props.tooltip ?? props.modTitle} data-tootip-position="bottom left">
+                <img src={props.mainIconUrl} className="tabItemIcon" />
+                <div className="titleContainer">
+                    <div className="tabName">{props.modTitle}</div>
+                    <div className="tabSubName">{props.subtitle}</div>
                 </div>
-                <TabList className="mainMenuStrip">
-                    <GameScrollComponent>
-                        {this.props.items.map((x, i) => <Tab key={i} className="mainMenuStripTab" data-tooltip={x.name} data-tootip-position="top left">
-                            {!x.tintedIcon && <img src={x.iconUrl} className="tabItemIcon" />}
-                            {x.tintedIcon && <div className="tabItemIcon" style={{ maskImage: `url(${x.iconUrl})` }} />}
-                            <div className="tabName">{x.name}</div>
-                        </Tab>)}
-                    </GameScrollComponent>
-                </TabList>
-                {this.props.items.map((x, i) => <TabPanel className="mainMenuStripContent" key={i}>{x.panelContent}</TabPanel>)}
-            </Tabs>
-        </>
-    }
+            </div>
+            <TabList className="mainMenuStrip">
+                <GameScrollComponent>
+                    {props.items.map((x, i) => <Tab key={i} className="mainMenuStripTab" data-tooltip={x.name} data-tootip-position="top left">
+                        {!x.tintedIcon && <img src={x.iconUrl} className="tabItemIcon" />}
+                        {x.tintedIcon && <div className="tabItemIcon" style={{ maskImage: `url(${x.iconUrl})` }} />}
+                        <div className="tabName">{x.name}</div>
+                    </Tab>)}
+                </GameScrollComponent>
+            </TabList>
+            {props.items.map((x, i) => <TabPanel className="mainMenuStripContent" key={i}>{x.panelContent}</TabPanel>)}
+        </Tabs>
+    </>
 }
